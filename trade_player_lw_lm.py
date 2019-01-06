f525c930ceceeca18ef71b191343f03587f9d888
bruceware <ducvu1386@gmail.com>
2019-01-06 14:59:58 +0700

    Update trade_player_lw_lm.py

--- trade_player_lw_lm.py
+++ trade_player_lw_lm.py
@@ -1,7 +1,9 @@
 import fut
 session = fut.Core('xxx@gmail.com','xxx','test',platform='ps4',debug=True)
 count=0
+session.tradepileClear()
 a = session.tradepile_size - len(session.tradepile())
+print("so luong card can send to tranfer %s" % a)
 items = session.watchlist()
 for x in items:
         if x['tradeId'] == -1:
@@ -13,16 +15,18 @@ for x in items:
         print(flag)
         if flag:
                 session.applyConsumable(x['id'], 5003065)
-                count += 1
                 print("count:%d" % (count))
                 print("sent card to tradepile")
                 session.sendToTradepile(x['id'])
                 session.sell(x['id'], 1200, 1300)
+                count += 1
         else:
                 a = session.club(defId='164985')
                 if a[0]['position'] == 'LW' :
                         session.applyConsumable(a[0]['id'], 5003065)
                 session.sendToTradepile(a[0]['id'])
-                session.sell(a[0]['id'], 1200, 1300)                
-print("wait for 5,10s ...")
+                session.sell(a[0]['id'], 1200, 1300)
+                count += 1
+print("done ...")
+session.relist()
 session.logout()
