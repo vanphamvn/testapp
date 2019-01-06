4ecd8e9df2a6c0c523f116bd8703248c131c7e78
bruceware <ducvu1386@gmail.com>
2019-01-06 22:24:03 +0700

    Update and rename vu_buy_lw_lm.py to buy_lw_lm_200.py

--- buy_lw_lm_200.py
+++ buy_lw_lm_200.py
@@ -3,8 +3,9 @@ import time
 import fut
 session = fut.Core('xxx@gmail.com','xxx','test',platform='ps4',debug=True)
 count=0
+i=0
 while True:
-        items = session.searchAuctions('training',max_buy=200, start=20,category='position',position='LW-LM')
+        items = session.searchAuctions('training',max_buy=200, start=i,category='position',position='LW-LM')
         for x in items:
                 if count>40: break
                 flag=session.bid(x['tradeId'], 200)
@@ -15,6 +16,7 @@ while True:
                                 count += 1
                                 print("count:%d" % (count))
         print("wait for 5,10s ...")
+        i += 20
         time.sleep(random.randint(10.15))
 session.logout()
 
