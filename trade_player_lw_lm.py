869d68ebddcb7254dc7f294739741d4bfe6fe91b
bruceware <ducvu1386@gmail.com>
2019-01-07 08:28:51 +0700

    Update trade_player_lw_lm.py

--- trade_player_lw_lm.py
+++ trade_player_lw_lm.py
@@ -25,8 +25,8 @@ for x in items:
                         count += 1
                         print("da send card thanh cong to Transfer, so luong %s" % count) 
                 else:
-                        a = session.club(defId='177326') # Nho tim defIf player put vo day
-                        if a[0]['position'] == 'LW' :
+                        b = session.club(defId=x['assetId']) # Nho tim defIf player put vo day
+                        if b[0]['position'] == 'LW' :
                                 session.applyConsumable(a[0]['id'], 5003065)
                         session.sendToTradepile(a[0]['id'])
                         session.sell(a[0]['id'], 1300, 1400)
