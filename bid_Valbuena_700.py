5dd354a5c49a16bcd8deb160b82ceb8737133596
bruceware <ducvu1386@gmail.com>
2019-01-07 08:33:39 +0700

    Update bid_Valbuena_700.py

--- bid_Valbuena_700.py
+++ bid_Valbuena_700.py
@@ -8,10 +8,11 @@ if len(wItems) > 0:
 		if a['tradeId'] == -1:
 			session.quickSell(a['id'])
 count=0
+i=0
 size = session.watchlist_size - len(wItems)
 print("so luong card can bid %s" % (size))
 while True:
-	items = session.searchAuctions('player',max_price=700, min_buy = 900, start=20,defId=177326)
+	items = session.searchAuctions('player',max_price=700, min_buy = 900, start=i,defId=177326)
 	for x in items:
 		if x['currentBid']>=700 :break
 		if count>=size: break
@@ -21,5 +22,6 @@ while True:
 			count += 1
 			print("count:%d" % (count))
 	print("wait for 5,10s ...")
+	i += 20
 	time.sleep(random.randint(10,15))
 session.logout(); # Need to be fixed here
