e8e447e4f098016e8268143c6d531508702384b8
bruceware <ducvu1386@gmail.com>
2019-01-07 12:41:17 +0700

    Update trade_player_lw_lm.py

--- trade_player_lw_lm.py
+++ trade_player_lw_lm.py
@@ -2,30 +2,36 @@ import fut
 session = fut.Core('xxx@gmail.com','xxx','test',platform='ps4',debug=True)
 count=0
 a=0
+session.relist()
+soldItems = session.tradepile()
+for x in soldItems:
+        if x['tradeState'] == 'closed':
+                session.tradepileClear()
+                break;
 a = session.tradepile_size - len(session.tradepile())
 print("so luong card can send to tranfer %s" % a)
 items = session.watchlist()
-for x in items:
-        if x['tradeId'] == -1:
-                session.quickSell(x['id'])
-                print("special case : xoa expired card %s" % (x['id']))
+for y in items:
+        if y['tradeId'] == -1:
+                session.quickSell(y['id'])
+                print("special case : xoa expired card %s" % (y['id']))
                 break
-        if x['bidState'] == 'outbid':
-                session.watchlistDelete(x['tradeId'])
+        if y['bidState'] == 'outbid':
+                session.watchlistDelete(y['tradeId'])
                 print("xoa card outbid")
         if count>=a: break
-        if all([x['bidState'] == 'highest', x['tradeState'] == 'closed']):
-                flag=session.sendToClub(x['id'])
+        if all([y['bidState'] == 'highest', y['tradeState'] == 'closed']):
+                flag=session.sendToClub(y['id'])
                 print(flag)
                 if flag:
-                        session.applyConsumable(x['id'], 5003065)
+                        session.applyConsumable(y['id'], 5003065)
                         print("sent card to tradepile")
-                        session.sendToTradepile(x['id'])
-                        session.sell(x['id'], 1300, 1400)
+                        session.sendToTradepile(y['id'])
+                        session.sell(y['id'], 1300, 1400)
                         count += 1
                         print("da send card thanh cong to Transfer, so luong %s" % count) 
                 else:
-                        b = session.club(defId=x['assetId']) # Nho tim defIf player put vo day
+                        b = session.club(defId=y['assetId']) # Nho tim defIf player put vo day
                         if b[0]['position'] == 'LW' :
                                 session.applyConsumable(b[0]['id'], 5003065)
                         session.sendToTradepile(b[0]['id'])
@@ -33,6 +39,4 @@ for x in items:
                         count += 1
                         print("da send card thanh cong to Transfer, so luong %s" % count)
 print("done ...")
-session.relist()
-#session.tradepileClear() # can xu li them, neu co the sold thi run it
 session.logout()
